82720c8e99088f5fdabda20ef81d2e0c838154b2
justdan6 <justdan2000@gmail.com>
2023-06-22 20:56:46 -0400

    Quick readme update

--- run-docker.sh
+++ run-docker.sh
@@ -16,4 +16,4 @@ docker run --rm \
   -v "$PWD/tests/localhost.crt":/etc/nginx/ssl/localhost.crt:ro \
   -v "$PWD/tests/localhost.key":/etc/nginx/ssl/localhost.key:ro \
  --name test_nginx \
-  -t macbre/nginx
+  -t dockernginxhttp3quictls:latest
